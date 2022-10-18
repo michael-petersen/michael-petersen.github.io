85a4b521a9f0da2afcd7ded20c6effefab0d8015
michael-petersen <petersen.michael.s@gmail.com>
2022-10-18 11:17:06 +0100

    Finish updating local dwarfs

--- roe/lurg/simonreview/js/figures.js
+++ roe/lurg/simonreview/js/figures.js
@@ -29,16 +29,15 @@ var fig1 = d3.select("#one")
 
 // set up x
 var xValue1 = function(d) { return d.r2;},
-    xScale1 = d3.scaleLog().domain([0.8,3000]).range([0, width])
+    xScale1 = d3.scaleLog().domain([0.67,3000]).range([0, width])
     xMap1 = function(d) { return xScale1(xValue1(d));},
     xAxis1 = d3.axisBottom(xScale1).ticks(10, "~s");
 
 // set up y
 var yValue1 = function(d) { return d.MV;},
-    yScale1 = d3.scaleLinear().domain([1,-14]).range([height, 0]),
+    yScale1 = d3.scaleLinear().domain([2,-14]).range([height, 0]),
     yMap1 = function(d) { return yScale1(yValue1(d));},
     yAxis1 = d3.axisLeft(yScale1);
-
 
 // set up colors
 var colorscale1 = d3.scaleSequential(d3.interpolateBlues).domain([0,1]),
@@ -65,6 +64,19 @@ var colorscale1 = d3.scaleSequential(d3.interpolateBlues).domain([0,1]),
             { return colorscale1(0.8);}
           };
 
+// Handmade legend
+// https://d3-graph-gallery.com/graph/custom_legend.html
+fig1.append("circle").attr("cx",100).attr("cy",10).attr("r", 2.5).style("fill", "white").attr("class", "dot1a")
+fig1.append("text").attr("x", 120).attr("y", 10).text("Globular Clusters (Harris 2010)").style("font-size", "15px").attr("alignment-baseline","middle")
+fig1.append("circle").attr("cx",100).attr("cy",30).attr("r", 5.5).style("fill", colorscale2(0.8))
+fig1.append("text").attr("x", 120).attr("y", 30).text("Newly discovered ultrafaint clusters (Cerny 2022a,b)").style("font-size", "15px").attr("alignment-baseline","middle")
+fig1.append("circle").attr("cx",100).attr("cy",50).attr("r", 5.5).style("fill", colorscale2(0.4))
+fig1.append("text").attr("x", 120).attr("y", 50).text("LMC associations (Erkal & Belokurov 2020)").style("font-size", "15px").attr("alignment-baseline","middle")
+fig1.append("circle").attr("cx",100).attr("cy",70).attr("r", 5.5).style("fill", colorscale1(0.8))
+fig1.append("text").attr("x", 120).attr("y", 70).text("Classical dwarfs (Simon 2019)").style("font-size", "15px").attr("alignment-baseline","middle")
+fig1.append("circle").attr("cx",100).attr("cy",90).attr("r", 5.5).style("fill", colorscale1(0.2))
+fig1.append("text").attr("x", 120).attr("y", 90).text("Ultrafaint dwarfs (Simon 2019)").style("font-size", "15px").attr("alignment-baseline","middle")
+
 
 var gravG = 0.0000043009125; // gravitational constant, (km/s)^2 * kpc / Msun
 
@@ -77,101 +89,82 @@ var tooltip1 = d3.select("#one").append("div")
     .attr("class", "tooltip").style("opacity", 0);
 
 
-    // start the data access for GCs
-    d3.csv("harris10.csv",d3.autoType).then( function(data){
-      data.forEach(function(d) {
-        // convert data into numbers
-        d.MV = +d.MV
-        d.r2 = +d.rhalf
-        d.appmag = +d.vmag
-          d.dist = +d.helio_distance
-          d.sigma = +d.sigma
-      });
+// start the data access for GCs
+d3.csv("harris10.csv",d3.autoType).then( function(data){
+  data.forEach(function(d) {
+    // convert data into numbers
+    d.MV = +d.MV
+    d.r2 = +d.rhalf
+    d.appmag = +d.vmag
+    d.dist = +d.helio_distance
+    d.sigma = +d.sigma
+  });
 
-      // print example data to console for checking
-      console.log(data[0]);
+  // print example data to console for checking
+  console.log(data[0]);
 
-      gdata = data.filter(function(d) { return d.MV < 0. })
+  // only plot dwarfs above a certain brightness
+  gdata = data.filter(function(d) { return d.MV < 2. })
 
-      //console.log(data[0]['name']);
+  // lines of constant surface brightness
+  // if extending: 10^(log10(oldxmin) -  (newxmin-oldxmin)/(oldymax-oldymin)/(log10(oldxmax)-log10(oldxmin)))
+  fig1.selectAll('line1.brightness')
+     .data(data)
+     .enter().append('line')
+     .attr('class', 'brightness')
+     .attr('x1', function(d) { return xScale1(0.67); })
+     .attr('x2', function(d) { return xScale1(1800.); })
+     .attr('y1', function(d) { return yScale1(2.); })
+     .attr('y2', function(d) { return yScale1(-14.); });
 
-      // https://www.d3-graph-gallery.com/graph/shape.html
-      // http://bl.ocks.org/enthal/1726550
-      var datal = [{x: 10}, {x: 100}, {x: 300}, {x: 500}]
+  fig1.selectAll('line2.brightness')
+     .data(data)
+     .enter().append('line')
+     .attr('class', 'brightness')
+     .attr('x1', function(d) { return xScale1(1.686); })
+     .attr('x2', function(d) { return xScale1(3000.); })
+     .attr('y1', function(d) { return yScale1(2.); })
+     .attr('y2', function(d) { return yScale1(-13.25); });
 
-      // create svg element:
-      var svg = d3.select("#curve").append("svg").attr("width", 800).attr("height", 200)
+  fig1.selectAll('line3.brightness')
+     .data(data)
+     .enter().append('line')
+     .attr('class', 'brightness')
+     .attr('x1', function(d) { return xScale1(3.756); })
+     .attr('x2', function(d) { return xScale1(3000.); })
+     .attr('y1', function(d) { return yScale1(2.); })
+     .attr('y2', function(d) { return yScale1(-11.25); });
 
-      // prepare a helper function
-      var curveFunc1 = d3.line()
-        .curve(d3.curveBasis)              // This is where you define the type of curve. Try curveStep for instance.
-        .x(function(d) { return xScale3(d.x) })
-        .y(function(d) { return yScale3(5*Math.log10(d.x)+2.7) })
-
-      var curveFunc2 = d3.line()
-        .curve(d3.curveBasis)              // This is where you define the type of curve. Try curveStep for instance.
-        .x(function(d) { return xScale3(d.x) })
-        .y(function(d) { return yScale3(5*Math.log10(d.x)+4) })
-
-
-      // lines of faintness
-        fig1.selectAll('line1.brightness')
-           .data(data)
-           .enter().append('line')
-           .attr('class', 'brightness')
-           .attr('x1', function(d) { return xScale1(1.8); })
-           .attr('x2', function(d) { return xScale1(1800.); })
-           .attr('y1', function(d) { return yScale1(0.); })
-           .attr('y2', function(d) { return yScale1(-14.); });
-
-        fig1.selectAll('line2.brightness')
-           .data(data)
-           .enter().append('line')
-           .attr('class', 'brightness')
-           .attr('x1', function(d) { return xScale1(4.5); })
-           .attr('x2', function(d) { return xScale1(3000.); })
-           .attr('y1', function(d) { return yScale1(0.); })
-           .attr('y2', function(d) { return yScale1(-13.25); });
-
-        fig1.selectAll('line3.brightness')
-           .data(data)
-           .enter().append('line')
-           .attr('class', 'brightness')
-           .attr('x1', function(d) { return xScale1(10.3); })
-           .attr('x2', function(d) { return xScale1(3000.); })
-           .attr('y1', function(d) { return yScale1(0.); })
-           .attr('y2', function(d) { return yScale1(-11.25); });
-
-        fig1.selectAll('line4.brightness')
-           .data(data)
-           .enter().append('line')
-           .attr('class', 'brightness')
-           .attr('x1', function(d) { return xScale1(28); })
-           .attr('x2', function(d) { return xScale1(3000.); })
-           .attr('y1', function(d) { return yScale1(0.); })
-           .attr('y2', function(d) { return yScale1(-9.25); });
+  fig1.selectAll('line4.brightness')
+     .data(data)
+     .enter().append('line')
+     .attr('class', 'brightness')
+     .attr('x1', function(d) { return xScale1(10.19); })
+     .attr('x2', function(d) { return xScale1(3000.); })
+     .attr('y1', function(d) { return yScale1(2.); })
+     .attr('y2', function(d) { return yScale1(-9.25); });
 
 
-        // draw dots
-        fig1.selectAll(".dot")
-            .data(gdata)
-            .enter().append("circle")
-            .attr("class", "dot1a")
-            .attr("r", 2.5)
-            .attr("cx", xMap1)
-            .attr("cy", yMap1)
-      	.style("fill", "white")
-      	.on("mouseover", function(d) {
-                tooltip.transition().duration(200).style("opacity", .9);
-                tooltip.html(d['name'])
-                     .style("left", (d3.event.pageX + 5) + "px")
-                     .style("top", (d3.event.pageY - 28) + "px");
-                })
-            .on("mouseout", function(d) {
-                tooltip.transition().duration(500).style("opacity", 0);
-                });
+  // draw Globular cluster points
+  fig1.selectAll(".dot")
+      .data(gdata)
+      .enter().append("circle")
+      .attr("class", "dot1a")
+      .attr("r", 2.5)
+      .attr("cx", xMap1)
+      .attr("cy", yMap1)
+    	.style("fill", "white")
+    	.on("mouseover", function(d) {
+              tooltip.transition().duration(200).style("opacity", .9);
+              tooltip.html(d['name'])
+                   .style("left", (d3.event.pageX + 5) + "px")
+                   .style("top", (d3.event.pageY - 28) + "px");
+              })
+      .on("mouseout", function(d) {
+          tooltip.transition().duration(500).style("opacity", 0);
+          });
 
-    });
+});
 
 
 // start the data access
@@ -214,21 +207,25 @@ d3.csv("simondwarfs.csv",d3.autoType).then( function(data){
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis1)
+      .style("font-size", "15px")
   fig1.append("text")
       .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
       .style("text-anchor", "middle")
+      .style("font-size", "15px")
       .text("Half-Light Radius [pc]");
 
   // y-axis
   fig1.append("g")
       .attr("class", "y axis")
       .call(yAxis1)
+      .style("font-size", "15px")
   fig1.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left)
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
+      .style("font-size", "15px")
       .text("Absolute V Magnitude");
 
   // draw dots, and make them mouseable
