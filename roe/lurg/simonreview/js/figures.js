52df6936827eb9ce32d3c2db8bff4354f6f672fe
michael-petersen <petersen.michael.s@gmail.com>
2023-11-28 13:52:26 +0000

    update dwarf galaxy data

--- roe/lurg/simonreview/js/figures.js
+++ roe/lurg/simonreview/js/figures.js
@@ -35,22 +35,31 @@ var xValue1 = function(d) { return d.r2;},
 
 // set up y
 var yValue1 = function(d) { return d.MV;},
-    yScale1 = d3.scaleLinear().domain([2,-14]).range([height, 0]),
+    yScale1 = d3.scaleLinear().domain([3,-14]).range([height, 0]),
     yMap1 = function(d) { return yScale1(yValue1(d));},
     yAxis1 = d3.axisLeft(yScale1);
 
 // set up colors
 var colorscale1 = d3.scaleSequential(d3.interpolateBlues).domain([0,1]),
     colorscale2 = d3.scaleSequential(d3.interpolateReds).domain([0,1]),
+
     cValue1 = function(d) {
-      if ( d.Dwarf=="Centaurus I" ||  d.Dwarf=="Eridanus IV" ||  d.Dwarf=="Pegasus IV" || d.Dwarf=="Delve 3" || d.Dwarf=="Delve 4" || d.Dwarf=="Virgo II" || d.Dwarf=="Delve 5" || d.Dwarf=="Bootes V" || d.Dwarf=="Leo Minor I")
-        { return colorscale2(0.8);}
+      if ( d.Dwarf=="Centaurus I" ||  d.Dwarf=="Eridanus IV" ||  d.Dwarf=="Pegasus IV" || d.Dwarf=="Delve 3" || d.Dwarf=="Delve 4" || d.Dwarf=="Virgo II" || d.Dwarf=="Delve 5" || d.Dwarf=="Bootes V" || d.Dwarf=="Leo Minor I" || d.Dwarf=="UNIONS 1")
+        { // Newly discovered satellites
+          return colorscale2(0.8);
+        }
       else if (d.Dwarf=="Carina II" || d.Dwarf=="Carina III" || d.Dwarf=="Horologium I" || d.Dwarf=="Hydrus I" || d.Dwarf=="Phoenix II" || d.Dwarf=="Reticulum II")
-        { return colorscale2(0.4);}
+        { // LMC satellites
+          return colorscale2(0.4);
+        }
       else if (d.MV>-7.5)
-        { return colorscale1(0.2);}
+        { // classical dwarfs
+          return colorscale1(0.2);
+        }
 			else
-        { return colorscale1(0.8);}
+        { // faint dwarfs
+          return colorscale1(0.8);
+        }
       },
 
     cValue2 = function(d) {
@@ -69,7 +78,7 @@ var colorscale1 = d3.scaleSequential(d3.interpolateBlues).domain([0,1]),
 fig1.append("circle").attr("cx",100).attr("cy",10).attr("r", 2.5).style("fill", "white").attr("class", "dot1a")
 fig1.append("text").attr("x", 120).attr("y", 10).text("Globular Clusters (Harris 2010)").style("font-size", "15px").attr("alignment-baseline","middle")
 fig1.append("circle").attr("cx",100).attr("cy",30).attr("r", 5.5).style("fill", colorscale2(0.8))
-fig1.append("text").attr("x", 120).attr("y", 30).text("Newly discovered Ultrafaints (Cerny 2022a,b, Heiger 2023)").style("font-size", "15px").attr("alignment-baseline","middle")
+fig1.append("text").attr("x", 120).attr("y", 30).text("Newly discovered Ultrafaints (Cerny 2022a,b, Heiger 2023, Smith 2023)").style("font-size", "15px").attr("alignment-baseline","middle")
 fig1.append("circle").attr("cx",100).attr("cy",50).attr("r", 5.5).style("fill", colorscale2(0.4))
 fig1.append("text").attr("x", 120).attr("y", 50).text("LMC associations (Erkal & Belokurov 2020)").style("font-size", "15px").attr("alignment-baseline","middle")
 fig1.append("circle").attr("cx",100).attr("cy",70).attr("r", 5.5).style("fill", colorscale1(0.8))
@@ -101,7 +110,7 @@ d3.csv("harris10.csv",d3.autoType).then( function(data){
   });
 
   // print example data to console for checking
-  console.log(data[0]);
+  //console.log(data[0]);
 
   // only plot dwarfs above a certain brightness
   gdata = data.filter(function(d) { return d.MV < 2. })
@@ -190,6 +199,9 @@ d3.csv("simondwarfs.csv",d3.autoType).then( function(data){
     d.umhalf      = 5.*0.001*(+d.r2+d.dr2)*(+d.sigma+d.usigma)*(+d.sigma+d.usigma)/(2*gravG)
     d.dmhalf      = 5.*0.001*(+d.r2+d.ur2)*(+d.sigma+d.dsigma)*(+d.sigma+d.dsigma)/(2*gravG)
     //console.log(d.mhalf,d.umhalf,d.r2+d.dr2,d.sigma+d.dsigma)
+    d.sigFeH      = +d.sigFeH
+    d.usigFeH    = +d.usigFeH
+    d.dsigFeH    = +d.dsigFeH
     d.masslum     = d.mhalf/d.luminosity
     d.umasslum    = (+d.mhalf+d.umhalf)/d.luminosity
     d.dmasslum    = (+d.mhalf+d.dmhalf)/d.luminosity
