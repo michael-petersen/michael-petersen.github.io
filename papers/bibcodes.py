124239b576ac72b090a238ee5140256669713bfb
michael-petersen <petersen.michael.s@gmail.com>
2024-12-30 10:46:04 +0000

    small project updates

---
+++ papers/bibcodes.py
@@ -0,0 +1,50 @@
+
+def get_first_author_bibcodes():
+    # First author bibcodes
+    return [
+        "2016MNRAS.463.1952P",
+        "2016PhRvD..94l3013P",
+        "2019MNRAS.488.1462P",
+        "2019MNRAS.490.3616P",
+        "2020MNRAS.494L..11P",
+        "2021MNRAS.500..838P",
+        "2021NatAs...5..251P",
+        "2022MNRAS.510.6201P",
+        "2022MNRAS.514.1266P",
+        "2024MNRAS.530.4378P",
+        "2024MNRAS.531..751P",
+        "exp: a Python/C++ package for basis function expansion methods in galactic dynamics"
+        ]
+
+def get_student_led_bibcodes():
+    return [
+        "2022MNRAS.512..160R",
+        "2022MNRAS.513L..46D",
+        "2023MNRAS.518..774L",
+        "2023MNRAS.521.1757J",
+        "2024MNRAS.531.3524Y",
+        "{\tt commensurability}: a Python package for classifying astronomical orbits based on their toroid volume",
+        "2024arXiv241111972G"
+        ]
+
+def get_coauthor_bibcodes():
+    return [
+        "2014ApJ...792...64B",
+        "2021MNRAS.501.5408W",
+        "2021MNRAS.508L..26P",
+        "2024arXiv240207986H",
+        "2024JOSS....9.6906N"
+        ]
+
+def get_collaborative_bibcodes():
+    # collaborative bibcodes
+    return [
+        "2009ApJ...701..306E",
+        "2023ApJ...942...18C",
+        "2023MNRAS.518.4138E",
+        "2023ApJ...946...10P",
+        "2023MNRAS.520.4779L",
+        "2024MNRAS.532.2657B",
+        "2024arXiv241215033T",
+        "The Gaia Phase Space spiral in angular momentum"
+        ]
--- papers/queryads.py
+++ papers/queryads.py
@@ -2,10 +2,7 @@ import ads
 import os
 from datetime import datetime
 
-# todo
-# 1. Add h-index calculator
-# 2. Add printout of total citations
-
+import bibcodes as biblog
 
 # Retrieve ADS API token from environment variable
 ads_token = os.getenv("ADS_API_TOKEN")
@@ -40,21 +37,7 @@ total_citations = 0
 print('\\newcount\FirstAuthorCitations',file=f)
 print('\FirstAuthorCitations=0\n',file=f)
 
-# First author bibcodes
-bibcodes = [
-    "2016MNRAS.463.1952P",
-    "2016PhRvD..94l3013P",
-    "2019MNRAS.488.1462P",
-    "2019MNRAS.490.3616P",
-    "2020MNRAS.494L..11P",
-    "2021MNRAS.500..838P",
-    "2021NatAs...5..251P",
-    "2022MNRAS.510.6201P",
-    "2022MNRAS.514.1266P",
-    "2024MNRAS.530.4378P",
-    "2024MNRAS.531..751P",
-    "exp: a Python/C++ package for basis function expansion methods in galactic dynamics"
-]
+bibcodes = biblog.get_first_author_bibcodes()
 
 varnames = []
 nfirstauthor = len(bibcodes)
@@ -83,17 +66,8 @@ print('\n',file=f)
 print('\\newcount\StudentAuthorCitations',file=f)
 print('\StudentAuthorCitations=0\n',file=f)
 
-# List of Bibcodes for the papers you want to query
-# You can find these bibcodes in ADS for each paper
-bibcodes = [
-    "2022MNRAS.512..160R",
-    "2022MNRAS.513L..46D",
-    "2023MNRAS.518..774L",
-    "2023MNRAS.521.1757J",
-    "2024MNRAS.531.3524Y",
-    "{\tt commensurability}: a Python package for classifying astronomical orbits based on their toroid volume",
-    "2024arXiv241111972G"
-]
+# get student-led bibcodes
+bibcodes = biblog.get_student_led_bibcodes()
 
 varnames = []
 nstudentauthor = len(bibcodes)
@@ -123,15 +97,8 @@ print('\n',file=f)
 print('\\newcount\CoauthorCitations',file=f)
 print('\CoauthorCitations=0\n',file=f)
 
-# List of Bibcodes for the papers you want to query
-# You can find these bibcodes in ADS for each paper
-bibcodes = [
-    "2014ApJ...792...64B",
-    "2021MNRAS.501.5408W",
-    "2021MNRAS.508L..26P",
-    "2024arXiv240207986H",
-    "2024JOSS....9.6906N"
-]
+# get coauthor bibcodes
+bibcodes = biblog.get_coauthor_bibcodes()
 
 varnames = []
 ncoauthor = len(bibcodes)
@@ -161,16 +128,8 @@ print('\n',file=f)
 print('\\newcount\CollaboratorCitations',file=f)
 print('\CollaboratorCitations=0\n',file=f)
 
-# List of Bibcodes for the papers you want to query
-# You can find these bibcodes in ADS for each paper
-bibcodes = [
-    "2009ApJ...701..306E",
-    "2023ApJ...942...18C",
-    "2023MNRAS.518.4138E",
-    "2023ApJ...946...10P",
-    "2023MNRAS.520.4779L",
-    "2024MNRAS.532.2657B"
-]
+
+bibcodes = biblog.get_collaborative_bibcodes()
 
 varnames = []
 ncollaborator = len(bibcodes)
