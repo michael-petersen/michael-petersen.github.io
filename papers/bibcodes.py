43d60fcd2de767ae6e887160114a13b66eb4cb72
michael-petersen <petersen.michael.s@gmail.com>
2025-09-05 10:13:22 +0100

    update publication list

--- papers/bibcodes.py
+++ papers/bibcodes.py
@@ -2,6 +2,8 @@
 # try this search as a jumping off place for collecting bibcodes
 # https://ui.adsabs.harvard.edu/search/p_=0&q=author%3A%22Petersen%2C%20Michael%20S.%22&sort=date%20desc%2C%20bibcode%20desc
 
+# no numbering required, just add to the appropriate list
+
 def get_first_author_bibcodes():
     # First author bibcodes
     return [
@@ -28,7 +30,8 @@ def get_student_led_bibcodes():
         "2023MNRAS.521.1757J",
         "2024MNRAS.531.3524Y",
         "2025JOSS...10.7009S", # commensurability
-        "2025MNRAS.539..661G" # flex
+        "2025MNRAS.539..661G", # flex
+        "2025arXiv250804781Y" # MW-LMC reflex 2
         ]
 
 def get_coauthor_bibcodes():
@@ -38,7 +41,7 @@ def get_coauthor_bibcodes():
         "2021MNRAS.508L..26P",
         "2025MNRAS.538..998H",
         "2024JOSS....9.6906N", # lintsampler
-        "2025arXiv250609927F", # retrograde stars
+        "2025ApJ...989...70F", # retrograde stars
         ]
 
 def get_collaborative_bibcodes():
@@ -50,9 +53,10 @@ def get_collaborative_bibcodes():
         "2023ApJ...946...10P",
         "2023MNRAS.520.4779L",
         "2024MNRAS.532.2657B",
-        "2024arXiv241215033T",
-        "2025arXiv250104095Y",
+        "2025ApJ...986..203T", # KK linear response
+        "2025arXiv250104095Y", # angular momentum spiral
         "2025A&A...697A.214B", # dark matter spirals
-        "2025arXiv250420133A", # mssa on fire
-        "2025arXiv250613636T" # krios paper
+        "2025ApJ...988..190A", # mssa on fire
+        "2025arXiv250613636T", # krios paper
+        "2025arXiv250903424J" # S3 stream paper
         ]
